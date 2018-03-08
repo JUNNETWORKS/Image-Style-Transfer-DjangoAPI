c29c7795ac2b0da770ebf849ec3a3bbaf3f32f3a
yahooshiken <hoshimure.47@gmail.com>
2018-03-09 00:07:18 +0900

    set url

--- cnn/urls.py
+++ cnn/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.api, name='api'),
+    path('', views.api, name='api'),
     path('line/', views.line, name='line')
 ]
